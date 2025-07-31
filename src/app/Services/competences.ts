f7cfe392f8fee6c1e2af3df495d97f2f312a91a5
omayma-gam <gamranioumayma@gmail.com>
2025-07-31 17:33:16 +0100

    quelque modification

--- src/app/Services/competences.ts
+++ src/app/Services/competences.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Competences {
+  private apiUrl = 'http://localhost:8084/Competence';
+  constructor(private http: HttpClient) {}
 
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  }
 
+  getCompetence(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl, { headers: this.getHeaders() });
+  }
+
+  deleteCompetence(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`, { headers: this.getHeaders() });
+  }
+
+  updateCompetence(id: number, trip: { destination: string; date: string }): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, trip, {
+      headers: this.getHeaders() });
+  }
 }
